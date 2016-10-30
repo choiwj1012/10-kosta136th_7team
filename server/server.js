261357bcd0a29155ddd15b24cd99d94ba5576d6f
WonJae Choi <choiwj1012@hanmail.net>
2016-10-30 12:58:45 +0900

    [add] router

--- server/server.js
+++ server/server.js
@@ -25,8 +25,44 @@ http.createServer(app).listen(3000, function(){
   console.log('starting web server ... http://localhost:3000');
 });
 
-// connection
+// connection test
 Controller.getArticleController().test();
 Controller.getBoardController().test();
 Controller.getLoginController().test();
 Controller.getQnaController().test();
+
+// router
+// 메인페이지 요청
+app.all('/', function(request, response){
+
+  response.redirect('view/index.html');
+
+});
+
+// article 글쓰기
+app.all('/articleWrite', function(request, response){
+
+  console.log('/articleWrite 요청받음');
+
+});
+
+// article 글목록
+app.all('/articleList', function(request, response){
+
+  console.log('/articleList 요청받음');
+
+});
+
+// article 선택글읽기
+app.all('/articleRead', function(request, response){
+
+  console.log('/articleRead 요청받음');
+
+});
+
+// article 선택글삭제
+app.all('/articleDelete', function(request, response){
+
+  console.log('/articleDelete 요청받음');
+
+});
